3d40c62d9e7d5e52325727024e4b16674cd15c79
Feranmi Akinwale <feranmiphileo@gmail.com>
2025-05-20 17:02:54 +0200

    Initialize Model and setup database

--- unittestgen/admin.py
+++ unittestgen/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TestSession
+
+admin.site.register(TestSession)
 
 # Register your models here.
--- unittestgen/models.py
+++ unittestgen/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class TestSession(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='test_sessions')
+    uploaded_code = models.FileField(upload_to='uploaded_code/')
+    generated_tests = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    notes = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"Session {self.id} by {self.user.username} on {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"  # pylint: disable = no-member
